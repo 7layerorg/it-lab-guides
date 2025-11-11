442be71b017b6832f49b6797a17cc65db403c34c
Lazio <7layerorg@gmail.com>
2025-11-11 12:14:06 +0000

    Modded Cluster Init

--- mongodb-cluster/install.sh
+++ mongodb-cluster/install.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-#ansible-playbookprep_systems.yml
+ansible-playbookprep_systems.yml
 ansible-playbook site.yml -vv
-#ansible-playbook init_cluster.yml 
-#ansible-playbook init_cluster_meta.yml
-#ansible-playbook init_cluster_data.yml
 
 
